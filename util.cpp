7cbe71d4a00134dca862de7a064b3d4705481075
rexim <reximkut@gmail.com>
2016-07-27 19:09:54 +0600

    Add minus operation for Color on SFML < 2.2 (#21)

--- util.cpp
+++ util.cpp
@@ -1,11 +1,23 @@
+#include <SFML/Config.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include "util.hpp"
 
 namespace sf {
+
 Color operator*(const Color &color, float f)
 {
-  return sf::Color(color.r * f,
-                   color.g * f,
-                   color.b * f);
+    return Color(color.r * f,
+                 color.g * f,
+                 color.b * f);
 }
+
+#if SFML_VERSION_MAJOR == 2 && SFML_VERSION < 2
+Color operator-(const Color& left, const Color& right)
+{
+    return Color(left.r - right.r,
+                 left.g - right.g,
+                 left.b - right.b);
 }
+#endif
+
+}  // namespacs sf
