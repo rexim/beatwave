3f01de614c863600b37f42f2923a32e182f4c2ea
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2016-07-28 14:37:27 +0300

    fix undefined preprocessor constant
    (sfml minor version)

--- util.hpp
+++ util.hpp
@@ -1,13 +1,20 @@
 #ifndef UTIL_HPP_
 #define UTIL_HPP_
 
+#ifndef SFML_VERSION_MAJOR
+#error IMPOSSIBRU!
+#endif
+#ifndef SFML_VERSION_MINOR
+#error IMPOSSIBRU!
+#endif
+
 #include <SFML/Config.hpp>
 
 namespace sf {
     class Color;
 
     Color operator*(const Color &color, float f);
-#if SFML_VERSION_MAJOR == 2 && SFML_VERSION < 2
+#if SFML_VERSION_MAJOR == 2 && SFML_VERSION_MINOR < 2
     Color operator-(const Color& left, const Color& right);
 #endif
 
