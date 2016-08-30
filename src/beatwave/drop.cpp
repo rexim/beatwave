31f3198d082e16921c7f8eee4d5d35ee4324b0ef
rexim <reximkut@gmail.com>
2016-08-30 21:57:15 +0600

    Add Drop class (#41)

---
+++ src/beatwave/drop.cpp
@@ -0,0 +1,21 @@
+#include <beatwave/drop.hpp>
+
+Drop::Drop(const sf::Color &color):
+    m_radius(0.0f),
+    m_position(sf::Vector2f(0.0f, 0.0f)),
+    m_color(color)
+{}
+
+void Drop::render(sf::RenderTarget *renderTarget) const
+{
+    sf::CircleShape circle(m_radius.value());
+    circle.setFillColor(m_color);
+    circle.setPosition(m_position.value() - sf::Vector2f(m_radius.value(), m_radius.value()));
+    renderTarget->draw(circle);
+}
+
+void Drop::tick(sf::Int32 deltaTime)
+{
+    m_radius.tick(deltaTime);
+    m_position.tick(deltaTime);
+}
---
+++ src/beatwave/drop.hpp
@@ -0,0 +1,27 @@
+#ifndef DROP_HPP_
+#define DROP_HPP_
+
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <core/animated.hpp>
+
+namespace sf {
+    class RenderTarget;
+}
+
+class Drop
+{
+public:
+    Drop(const sf::Color &color);
+
+    void render(sf::RenderTarget *renderTarget) const;
+    void tick(sf::Int32 deltaTime);
+
+private:
+    Animated<double> m_radius;
+    Animated<sf::Vector2f> m_position;
+
+    sf::Color m_color;
+};
+
+#endif  // DROP_HPP_
