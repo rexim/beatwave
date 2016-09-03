6b33ea3069e829a4a07b989ed234187acc708c12
rexim <reximkut@gmail.com>
2016-09-03 22:23:02 +0600

    Introduce set animation

---
+++ src/core/set.hpp
@@ -0,0 +1,46 @@
+#ifndef SET_HPP_
+#define SET_HPP_
+
+#include <iostream>
+
+template <typename State>
+class Set: public Animation<State>
+{
+public:
+    Set(const State &state):
+        m_state(state),
+        m_finished(false)
+    {}
+
+    virtual ~Set() override
+    {
+        std::cout << "~Set()" << std::endl;
+    }
+
+    virtual State nextState(const int32_t) override
+    {
+        m_finished = true;
+        return m_state;
+    }
+
+    virtual State getCurrentState() const override
+    {
+        return m_state;
+    }
+
+    virtual bool isFinished() const override
+    {
+        return m_finished;
+    }
+
+    virtual void reset() override
+    {
+        m_finished = false;
+    }
+
+private:
+    const State m_state;
+    bool m_finished;
+};
+
+#endif  // SET_HPP_
