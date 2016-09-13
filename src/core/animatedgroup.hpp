ae42cb348da670153b9103c371879aae2980c2e7
rexim <reximkut@gmail.com>
2016-09-13 21:50:54 +0600

    Extract AnimatedTuple header (#58)
    Move FIXME(#57)

--- src/core/animatedgroup.hpp
+++ src/core/animatedgroup.hpp
@@ -1,25 +1,8 @@
 #ifndef ANIMATEDGROUP_HPP_
 #define ANIMATEDGROUP_HPP_
 
-#include <tuple>
 #include <core/animated.hpp>
-
-template <typename ...Tp>
-using AnimatedTuple = std::tuple<Animated<Tp>...>;
-
-// FIXME(#57): generalize tuple iteration process
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-iterateTick(AnimatedTuple<Tp...>&, int32_t)
-{}
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-iterateTick(AnimatedTuple<Tp...>& properties, int32_t deltaTime)
-{
-    std::get<I>(properties).tick(deltaTime);
-    iterateTick<I + 1, Tp...>(properties, deltaTime);
-}
+#include <core/animatedtuple.hpp>
 
 template <typename ...Tp>
 class AnimatedGroup
