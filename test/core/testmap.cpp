b9892206979af851ab3730e37848810682415b3c
rexim <reximkut@gmail.com>
2016-10-18 22:55:45 +0600

    Add UT for Map::getCurrentValue (#111)

--- test/core/testmap.cpp
+++ test/core/testmap.cpp
@@ -8,8 +8,8 @@
 class CountAnimation: public Animation<int>
 {
 public:
-    CountAnimation():
-        currentValue(0)
+    CountAnimation(int initialValue):
+        currentValue(initialValue)
     {}
 
     virtual int nextValue(const int32_t) override
@@ -27,24 +27,32 @@ public:
         return false;
     }
 
-    virtual void reset(const int&) override
+    virtual void reset(const int &initialValue) override
     {
-        currentValue = 0;
+        currentValue = initialValue;
     }
 
 private:
     int currentValue;
 };
 
-TEST_CASE("Map::nextValue should map the value the wrapped animation", "[map]") {
-    const std::array<int, 5> expected = { 0, 2, 4, 6, 8 };
-
-    auto map = AnimationPtr<int>(
-        new Map<int, int>(AnimationPtr<int>(new CountAnimation()),
+AnimationPtr<int> makeCountMap(int initialValue)
+{
+    return AnimationPtr<int>(
+        new Map<int, int>(AnimationPtr<int>(new CountAnimation(initialValue)),
                           [](const int& x) { return x * 2; },
                           [](const int& x) { return x / 2; }));
+}
+
+TEST_CASE("Map::nextValue should map the value the wrapped animation", "[map]") {
+    const std::array<int, 5> expected = { 0, 2, 4, 6, 8 };
+    auto map = makeCountMap(0);
 
     for (int x: expected) {
         REQUIRE ( x == map->nextValue(10) );
+    }
+
+    for (size_t i = 0; i < 5; ++i) {
+        REQUIRE ( 10 == map->getCurrentValue() );
     }
 }
