9278dd16aeb3b072a42c1e1d016b6b55f73613a2
rexim <reximkut@gmail.com>
2016-11-02 02:51:59 +0600

    Implement sleep acroding to the specs

--- src/core/animations/sleep.hpp
+++ src/core/animations/sleep.hpp
@@ -1,6 +1,7 @@
 #ifndef CORE_ANIMATIONS_SLEEP_HPP_
 #define CORE_ANIMATIONS_SLEEP_HPP_
 
+#include <algorithm>
 #include <core/animation.hpp>
 
 template <typename Value>
@@ -9,31 +10,37 @@ class Sleep: public Animation<Value>
 public:
     Sleep(int32_t initialTimeout):
         m_initialTimeout(initialTimeout),
-        m_currenTimeout(initialTimeout)
+        m_currentTimeout(initialTimeout),
+        m_currentValue()
     {}
 
     virtual Value nextValue(const int32_t deltaTime) override
     {
-        return Value();
+        m_currentTimeout = std::max(0, m_currentTimeout - deltaTime);
+
+        return m_currentValue;
     }
 
     virtual Value getCurrentValue() const override
     {
-        return Value();
+        return m_currentValue;
     }
 
     virtual bool isFinished() const override
     {
-        return false;
+        return m_currentTimeout <= 0;
     }
 
     virtual void reset(const Value &value) override
     {
+        m_currentValue = value;
+        m_currentTimeout = m_initialTimeout;
     }
 
 private:
     const int32_t m_initialTimeout;
-    int32_t m_currenTimeout;
+    int32_t m_currentTimeout;
+    Value m_currentValue;
 };
 
 #endif  // CORE_ANIMATIONS_SLEEP_HPP_
