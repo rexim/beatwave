c4289257d75e00614b18862bd96767d4ed142894
rexim <reximkut@gmail.com>
2016-11-23 23:41:41 +0600

    Fix UTs for FloatColor (#126)

--- test/core/testfloatcolor.cpp
+++ test/core/testfloatcolor.cpp
@@ -13,7 +13,7 @@ const float PRECISION = 1e-6;
 
 TEST_CASE("FloatColor summation", "[floatcolor]") {
     const auto actualColor = FloatColor(10.0f, 20.0f, 30.0f, 1.0f) + FloatColor(1.0f, 2.0f, 3.0f, 1.0f);
-    const auto expectedColor = FloatColor(11.0f, 22.0f, 33.0f, 1.0f);
+    const auto expectedColor = FloatColor(11.0f, 22.0f, 33.0f, 2.0f);
     REQUIRE ( std::fabs(actualColor.r - expectedColor.r) < PRECISION );
     REQUIRE ( std::fabs(actualColor.g - expectedColor.g) < PRECISION );
     REQUIRE ( std::fabs(actualColor.b - expectedColor.b) < PRECISION );
@@ -22,7 +22,7 @@ TEST_CASE("FloatColor summation", "[floatcolor]") {
 
 TEST_CASE("FloatColor subtraction", "[floatcolor]") {
     const auto actualColor = FloatColor(10.0f, 20.0f, 30.0f, 1.0f) - FloatColor(1.0f, 2.0f, 3.0f, 1.0f);
-    const auto expectedColor = FloatColor(9.0f, 18.0f, 27.0f, 1.0f);
+    const auto expectedColor = FloatColor(9.0f, 18.0f, 27.0f, 0.0f);
     REQUIRE ( std::fabs(actualColor.r - expectedColor.r) < PRECISION );
     REQUIRE ( std::fabs(actualColor.g - expectedColor.g) < PRECISION );
     REQUIRE ( std::fabs(actualColor.b - expectedColor.b) < PRECISION );
@@ -31,7 +31,7 @@ TEST_CASE("FloatColor subtraction", "[floatcolor]") {
 
 TEST_CASE("FloatColor factor multiplication", "[floatcolor]") {
     const auto actualColor = FloatColor(10.0f, 20.0f, 30.0f, 1.0f) * 3.0f;
-    const auto expectedColor = FloatColor(30.0f, 60.0f, 90.0f, 1.0f);
+    const auto expectedColor = FloatColor(30.0f, 60.0f, 90.0f, 3.0f);
     REQUIRE ( std::fabs(actualColor.r - expectedColor.r) < PRECISION );
     REQUIRE ( std::fabs(actualColor.g - expectedColor.g) < PRECISION );
     REQUIRE ( std::fabs(actualColor.b - expectedColor.b) < PRECISION );
@@ -40,7 +40,7 @@ TEST_CASE("FloatColor factor multiplication", "[floatcolor]") {
 
 TEST_CASE("FloatColor add operation", "[floatcolor]") {
     const auto deltaColor = FloatColor(1.0f, 2.0f, 3.0f, 1.0f);
-    const auto expectedColor = FloatColor(11.0f, 22.0f, 33.0f, 1.0f);
+    const auto expectedColor = FloatColor(11.0f, 22.0f, 33.0f, 2.0f);
     auto actualColor = FloatColor(10.0f, 20.0f, 30.0f, 1.0f);
     actualColor += deltaColor;
     REQUIRE ( std::fabs(actualColor.r - expectedColor.r) < PRECISION );
