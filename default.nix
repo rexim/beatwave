5dedbf8780d59362a8ab7a2e74a2dfcbec190507
rexim <reximkut@gmail.com>
2017-02-10 17:34:03 +0600

    Add PugiXML as a thirdparty dependency (#12)

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {}; {
   beatwaveEnv = clangStdenv.mkDerivation {
     name = "beatwave";
-    buildInputs = [ clangStdenv sfml cmake rtags doxygen ];
+    buildInputs = [ clangStdenv sfml cmake rtags doxygen pugixml ];
     CTEST_OUTPUT_ON_FAILURE = 1;
   };
 }
