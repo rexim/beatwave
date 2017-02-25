dc8ab4f0bfcf2c592b87f1bbe9df6b12f3297d25
rexim <reximkut@gmail.com>
2017-02-26 02:11:15 +0600

    Remove TODO (#141)

--- src/beatwave/level.cpp
+++ src/beatwave/level.cpp
@@ -4,17 +4,6 @@
 #include <beatwave/config.hpp>
 #include <beatwave/level.hpp>
 
-// TODO: Implement Level class
-//
-// The class should load level from XML file using PugiXML. Level
-// class should encapsulate Player, Tunnels and Path
-// correctors. Level::update() and Level::render() should propagate on
-// Player, Tunnels and Path correctors. See how to utilize
-// NestedGroups here.
-//
-// If the Game class is refactored correctly there should be no
-// difference between Game and Level classes.
-
 Level::Level(const char *levelFilePath)
 {
     pugi::xml_document doc;
